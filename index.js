570bdf6ebd78708c72b5a64ddd568100c594cf5b
Patrick <njorogepatrick739@gmail.com>
2024-07-23 16:30:10 +0300

    submit

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-function scuberGreetingForFeet(ride){
+function scuberGreetingForFeet (ride) {
 
-     if(ride<=400)
-      {
-     
-       return 'This one is on me!';
-}
-else if(ride > 400 && ride <=2000)  
-  {
-     
-     return 'That will be twenty bucks.';
+  if (ride<=400){
+    return 'This one is on me!';
   }
-  else if(ride > 2000 && ride <= 2500)
-  {
-    return 'I will gladly take your thirty bucks.';
+  else if (ride>400 && ride<=2000){
+   
+    return `That will be twenty bucks.`;
   }
-  else if(ride > 2500){
+  else if (ride>2000 && ride<=2500){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else {
     return 'No can do.';
   }
+
 }
-  // Write your code here!
 
 
-function ternaryCheckCity(city){
-  // Write your code here!
+function ternaryCheckCity (city) {
   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(tip){
-  // Write your code here!
-  return tip === 'generous' ? 'Thank you so much.' : tip === 'not as generous' ? 'Thank you.' : 'Bye.';
+
+function switchOnCharmFromTip (tip) {
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
